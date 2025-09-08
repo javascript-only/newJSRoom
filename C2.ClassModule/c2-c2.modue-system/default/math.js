deb2d3d8c4d30c8297f2846258f6ab46eb0a0258
Koras02 <wjdgh0727@gmail.com>
2025-09-08 14:07:56 +0900

    Chapter2.클래스와 모듈 - C2. 모듈 시스템(ESM, import / export)

---
+++ C2.ClassModule/c2-c2.modue-system/default/math.js
@@ -0,0 +1,5 @@
+// math.js
+export const PI = 3.14;
+export function add(a, b) {
+  return a + b;
+}
---
+++ C2.ClassModule/c2-c2.modue-system/esm/math.js
@@ -0,0 +1,10 @@
+// 여러 값 내보내기
+export const name = "Jake";
+export function jump() {
+  console.log("Jump!");
+}
+
+// 기본 (default) 내보내기
+export default function multiply(a, b) {
+  return a * b;
+}
